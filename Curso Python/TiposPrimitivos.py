5a6f6fb3d29db3cd4c89390609d8b43ac30b34dd
Jefferson Garcia <jemefox@hotmail.com>
2020-07-30 13:47:35 -0300

    Create TiposPrimitivos.py

---
+++ Curso Python/TiposPrimitivos.py
@@ -0,0 +1,8 @@
+# Curso Python #01 - Tipos Primitivos e Saída de Dados
+
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite mais um número: '))
+
+s = n1 + n2
+
+print('A Soma entre {} e {} vale {} ' .format(n1, n2, s))
