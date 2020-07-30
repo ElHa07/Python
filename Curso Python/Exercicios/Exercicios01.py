66ee18868a288e04f1b311c1ae8f960e49ba36d5
Jefferson Garcia <jemefox@hotmail.com>
2020-07-30 13:55:04 -0300

    Exercicios em Python
    # Exercício Python #001 - Somando dois números

---
+++ Curso Python/Exercicios/Exercicios01.py
@@ -0,0 +1,7 @@
+# Exercício Python #001 - Somando dois números
+
+n1 = int(input('Digite um numero: '))
+n2 = int(input('Digite outro valor: '))
+
+s =  n1 + n2 
+print('A soma ente {} e {} é igual a {}!' .format(n1, n2, s))
