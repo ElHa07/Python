e230ef7f9d9f7f2301176a97608dde29433e626a
Jefferson Garcia <jemefox@hotmail.com>
2020-07-30 14:30:34 -0300

    Update OperadoresAritimeticos.py

--- Curso Python/OperadoresAritimeticos.py
+++ Curso Python/OperadoresAritimeticos.py
@@ -1,1 +1,3 @@
 # Curso Python #02 - Operadores Aritméticos
+
+#Nessa aula, vamos aprender quais são os operadores aritméticos do Python e também sua ordem de precedência dentro de expressões matemáticas. Veja como funcionam os operadores de adição, subtração, multiplicação, divisão, exponenciação e quociente na linguagem Python.
