bbae5ce1109dc349c3d869b46bc3c9c3f6f2e21b
Jefferson Garcia <jemefox@hotmail.com>
2020-07-31 12:05:02 -0300

    Update Exercicios08.py

--- Curso Python/Aula02/Exercicios/Exercicios08.py
+++ Curso Python/Aula02/Exercicios/Exercicios08.py
@@ -1,3 +1,10 @@
 # Exercício Python #008 - Pintando Parede
 
-# Exercício Python 011: Faça um programa que leia a largura e a altura de uma parede em metros, calcule a sua área e a quantidade de tinta necessária para pintá-la, sabendo que cada litro de tinta pinta uma área de 2 metros quadrados.
+# Exercício: Faça um programa que leia a largura e a altura de uma parede em metros, calcule a sua área e a quantidade de tinta necessária para pintá-la, sabendo que cada litro de tinta pinta uma área de 2 metros quadrados.
+
+larg = float(input('Insira a Largura da parede: '))
+alt = float(input('Insira a Altura da parede: '))
+área = larg * alt
+tinta = área / 2
+print('Sua parede tem a dimensão de {} x {} e sua area é de {}m²'.format(larg,alt,área))
+print('Para pintar essa parede, você precisará de {:.1f}L de tinta !'.format(tinta))
