af52ff6d34478d9a5016be14d3961d085cd8a1d6
Jefferson Garcia <jemefox@hotmail.com>
2020-08-01 08:43:26 -0300

    Scartterplot

---
+++ Visualização de Dados com Python/GraficoDeScartterplot.py
@@ -0,0 +1,18 @@
+# Visualização de dados em Python
+import matplotlib.pyplot as plt
+
+x = [1,3,5,7,9]
+y = [2,3,7,1,0]
+
+titulo = "Scatterplot: Graficos De Dispersão"
+eixox = "Eixo X"
+eixox = "Eixo Y"
+
+plt.title(titulo)
+plt.xlabel("eixox")
+plt.ylabel("eixoy")
+
+plt.scatter(x,y, label="Meus Pontos", color="g", marker="h", s=100)
+plt.plot(x,y)
+plt.legend()
+plt.show()
--- Visualização de Dados com Python/GraficosDeBarra.py
+++ Visualização de Dados com Python/GraficosDeBarra.py
@@ -1,10 +1,13 @@
 import matplotlib.pyplot as plt
 
-x = [1,2,3,4,5]
-y = [2,3,5,7,0]
+x1 = [2,3,7,1,0]
+y1 = [2,3,5,7,10]
+
+x2 = [6,5,4,3,2]
+y2 = [2,3,5,7,0]
 
 #Titulo
-titulo = "grafico de Barras"
+titulo = "Grafico de Barras 2"
 eixox = "Eixo X"
 eixoy = "Eixo Y"
 
@@ -13,5 +16,7 @@ plt.title(titulo)
 plt.xlabel("Eixox")
 plt.ylabel("Eixosy")
 
-plt.bar(x,y)
+#plt 
+plt.bar(x1,y1, label="Grupo 1")
+plt.bar(x2,y2, label="Grupo 2")
 plt.show()
