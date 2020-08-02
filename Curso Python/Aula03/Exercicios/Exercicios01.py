76b0995a7078a7de542ae72335defabb1ae6467e
Jefferson Garcia <jemefox@hotmail.com>
2020-08-02 15:51:56 -0300

    Cateto e Hipotenusa

--- Curso Python/Aula03/Exercicios/Exercicios01.py
+++ Curso Python/Aula03/Exercicios/Exercicios01.py
@@ -4,11 +4,16 @@
 
 # Primeiro Exemplo 
 
+# num = float(input('Digite um Número: '))
+# print('O valor digitado foi {} e a sua porção inteira é {}'.format(num,int(num)))
+
+# Segundo Exemplo
+
 # import math
 # num = float(input('Digite um Número: '))
 # print('O valor digitado foi {} e a sua porção inteira é {}'.format(num,math.trunc(num)))
 
-# Segundo Exemplo
+# Terceiro Exemplo
 
 from math import trunc
 num = float(input('Digite um Número: '))
