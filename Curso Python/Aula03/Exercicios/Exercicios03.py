f8a710cdb4e043f36484994155aa80de6a0d2bd8
Jefferson Garcia <jemefox@hotmail.com>
2020-08-03 11:58:58 -0300

    Update Exercicios03.py

--- Curso Python/Aula03/Exercicios/Exercicios03.py
+++ Curso Python/Aula03/Exercicios/Exercicios03.py
@@ -2,11 +2,22 @@
 
 # Exercício: Faça um programa que leia um ângulo qualquer e mostre na tela o valor do seno, cosseno e tangente desse ângulo.
 
-import math
+# Primeiro Exemplo
+# import math
+# an = float(input('Digite o ângulo que você deseja: '))
+# seno = math.sin(math.radians(an))
+# print('O ângulo de {} tem o SENO de {:.2f}'.format(an,seno))
+# cosseno = math.cos(math.radians(an))
+# print('O ângulo de {} tem o COSSENO de {:.2f}'.format(an,cosseno))
+# print('O ângulo de {} tem a TANGENTE de {:.2f}'.format(an,tangente))
+
+# Segundo Exemplo
+
+from math import radians, sin, cos, tan
 an = float(input('Digite o ângulo que você deseja: '))
-seno = math.sin(math.radians(an))
-print('O ângulo de {} tem o SENO de {:.2f}'.format(an,seno))
-cosseno = math.cos(math.radians(an))
-print('O ângulo de {} tem o COSSENO de {:.2f}'.format(an,cosseno))
-tangente = math.tan(math.radians(an))
-print('O ângulo de {} tem a TANGENTE de {:.2f}'.format(an,tangente))
+seno = sin(radians(an))
+print('O ângulo de {} tem o SENO de {}'.format(an, seno))
+cosseno = cos(radians(an))
+print('O ângulo de {} tem o COSSENO de {:.2}'.format(an,cosseno))
+tangente = tan(radians(an))
+print('O ângulo de {} tem a TANGENTE de {:.2}'.format(an,tangente))
