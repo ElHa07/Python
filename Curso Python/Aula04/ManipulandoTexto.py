d07624095eb6d254fc82fcbadb8f563ca462dc50
Jefferson Garcia <jemefox@hotmail.com>
2020-08-31 14:09:05 -0300

    Update ManipulandoTexto.py

--- Curso Python/Aula04/ManipulandoTexto.py
+++ Curso Python/Aula04/ManipulandoTexto.py
@@ -2,4 +2,14 @@
 
 # As principais operações que vamos aprender são o Fatiamento de String, Análise com len(), count(), find(), transformações com replace(), upper(), lower(), capitalize(), title(), strip(), junção com join()
 
- 
+#Primeira Parte
+#texto = str('Curso em Python')
+#print(texto[:15:2])
+
+#Segunda Parte
+#texto = str('Curso em Video Python')
+#print(texto.upper().count('o'))
+
+#Terceiro Parte
+texto = str('Curso em Python')
+print(len(texto))
