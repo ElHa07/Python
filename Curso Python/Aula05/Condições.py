11a70999b05175eb1b37245a59664cf64beb6a91
Jefferson Garcia <jemefox@hotmail.com>
2020-09-25 12:02:14 -0300

    Condições

---
+++ Curso Python/Aula05/Condições.py
@@ -0,0 +1,15 @@
+#Curso Python #05 - Condições
+
+#Nessa aula, vamos aprender como utilizar estruturas #condicionais simples e compostas nos seus programas #em Python.
+
+#Condições
+#Simples e Compostas.
+
+tempo = int(input('Quantos anos tem seu carro?'))
+
+if tempo <= 3:
+    print('carro novo !')
+else:
+    print('carro velho!')
+print('--FIM--')
+
