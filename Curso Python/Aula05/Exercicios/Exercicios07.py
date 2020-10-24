48e8b8587b1908507619f1ea48d68663db1032ef
Jefferson Garcia <jemefox@hotmail.com>
2020-10-24 17:12:33 -0300

    Exercicios07

--- Curso Python/Aula05/Exercicios/Exercicios07.py
+++ Curso Python/Aula05/Exercicios/Exercicios07.py
@@ -3,3 +3,10 @@
 #Exercício Python 07: Escreva um programa que pergunte o salário de um funcionário e calcule o valor do seu aumento. Para salários superiores a R$1250,00, calcule um aumento de 10%. Para os inferiores ou iguais, o aumento é de 15%.
 
 #Primeiro Exemplo
+
+salário = float(input('Qual é a salário do funcionário? R$'))
+if salário <= 1250:
+    novo = salário + (salário * 15 / 100)
+else:
+    novo = salário + (salário * 10 / 100)
+print('Quem ganhava R${:.2f} passa a ganhar R${:.2f} agora.'.format(salário,novo))
