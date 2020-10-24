5df4e21fc24d4dc8d23682c5d43e02f8c13f1bb9
Jefferson Garcia <jemefox@hotmail.com>
2020-10-24 16:55:22 -0300

    Exercicios07

---
+++ Curso Python/Aula05/Exercicios/Exercicios07.py
@@ -0,0 +1,5 @@
+#Python #07 - Aumentos múltiplos
+
+#Exercício Python 07: Escreva um programa que pergunte o salário de um funcionário e calcule o valor do seu aumento. Para salários superiores a R$1250,00, calcule um aumento de 10%. Para os inferiores ou iguais, o aumento é de 15%.
+
+#Primeiro Exemplo
