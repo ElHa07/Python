68721bafa93b28407c2112a9d186b64869fced3f
Jefferson Garcia <jemefox@hotmail.com>
2020-11-13 20:28:16 -0300

    Exercicios06

---
+++ Curso Python/Aula06/Exercicios/Exercicios06.py
@@ -0,0 +1,28 @@
+#Python Exercício #06 - Classificando Atletas
+
+print('-=' * 30 )
+print('''Exercício: A Confederação Nacional de Natação precisa de um programa que leia o ano de nascimento de um atleta e mostre sua categoria, de acordo com a idade:
+[ Até 9 anos: MIRIM ]
+[ Até 14 anos: INFANTIL ]
+[ Até 19 anos: JÚNIOR ]
+[ Até 25 anos: SÊNIOR ]
+[ Acima de 25 anos: MASTER ]
+''')
+print('-=' * 30 )
+
+from datetime import date
+atual = date.today().year
+nascimento = int(input('Ano de Nascimento: '))
+idade = atual - nascimento
+print('O atleta tem {} anos.'.format(idade))
+if idade <= 9:
+    print('Classificação: MIRIN')
+elif idade <= 14:
+    print('Classificação: INFANTIL')
+elif idade <= 19:
+    print('Classificação: JUNIOR')
+elif idade <= 25:
+    print('Classificação: SENIOR')
+else:
+    print('Classificação: ! MASTER !')
+print('-=' * 30 )
