802caed132393fa43fea0c99918520bf6de2a069
Jefferson Garcia <jemefox007@gmail.com>
2020-11-23 15:55:54 -0300

    Exercicios10
    Jokenpô

---
+++ Curso Python/Aula06/Exercicios/Exercicios10.py
@@ -0,0 +1,42 @@
+# Exercício Python 10
+# Exercício: Crie um programa que faça o computador jogar Jokenpô com você.
+
+from random import randint
+itens = ('Pedra','Papel','Tesoura')
+computador = randint(0,2)
+print('''SUAS OPÇÕES:
+[ 0 ] PEDRA
+[ 1 ] PAPEL
+[ 2 ] TESOURA''')
+jogador = int(input('Qual é a sua jogada? ')) #Jogador tenta sua jogada !
+print('-=' * 11)
+print('Computador jogou {}'.format(itens[computador]))
+print('Jogador jogou {}'.format(itens[jogador]))
+print('-=' * 11)
+if computador == 0: # Computador Jogou Pedra
+    if jogador == 0:
+        print('EMPATE')
+    elif jogador == 1:
+        print('JOGADOR VENCE')
+    elif jogador == 2:
+        print('COMPUTADOR VENCE')
+    else:
+        print('JOGADOR INVALIDA!')
+elif computador == 1: # Computador Jogou Papel
+    if jogador == 0:
+        print('COMPUTADOR VENCE')
+    elif jogador == 1:
+        print('EMPATE')
+    elif jogador == 2:
+        print('JOGADOR VENCE')
+    else:
+        print('JOGADOR INVALIDA!')
+elif computador == 2: # Computador Jogou Tetoura
+    if jogador == 0:
+        print('JOGADOR VENCE')
+    elif jogador == 1:
+        print('COMPUTADOR VENCE')
+    elif jogador == 2:
+        print('EMPATE')
+    else:
+        print('JOGADA INVALIDA!')
