033736b4089cc4eb8f2cc6e215a36c46d422e0ad
Jefferson Garcia <jemefox007@gmail.com>
2020-11-23 16:01:22 -0300

    Exercicios10
    Jokenpô

--- Curso Python/Aula06/Exercicios/Exercicios10.py
+++ Curso Python/Aula06/Exercicios/Exercicios10.py
@@ -2,13 +2,19 @@
 # Exercício: Crie um programa que faça o computador jogar Jokenpô com você.
 
 from random import randint
+from time import sleep
 itens = ('Pedra','Papel','Tesoura')
 computador = randint(0,2)
 print('''SUAS OPÇÕES:
 [ 0 ] PEDRA
 [ 1 ] PAPEL
 [ 2 ] TESOURA''')
-jogador = int(input('Qual é a sua jogada? ')) #Jogador tenta sua jogada !
+jogador = int(input('Qual é a sua jogada? '))
+print(' JO ')
+sleep(1)
+print(' KEN ')
+sleep(1)
+print(' PO !!! ')
 print('-=' * 11)
 print('Computador jogou {}'.format(itens[computador]))
 print('Jogador jogou {}'.format(itens[jogador]))
