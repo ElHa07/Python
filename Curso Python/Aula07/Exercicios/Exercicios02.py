5c32a0b2f468095a5831257a240466a60a536cf9
Jefferson Garcia <jemefox007@gmail.com>
2020-11-24 19:55:00 -0300

    Exercicios02
    Contagem de pares

---
+++ Curso Python/Aula07/Exercicios/Exercicios02.py
@@ -0,0 +1,15 @@
+#Exercício Python 02
+
+#Exercício: Crie um programa que mostre na tela todos os números pares que estão no intervalo entre 1 e 50
+
+#Primeiro Exemplo
+
+#for cont in range(0, 51, 2):
+#    print(cont, end=' ')
+#print('Acabou')
+
+#Segundo Exemplo
+
+#for cont in range(50, 0, -2):
+#    print(cont, end=' ')
+#print('Acabou')
