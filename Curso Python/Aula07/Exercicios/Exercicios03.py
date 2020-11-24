07e438656f0a4d48ab20fe39ef83ac2b6b1ad1b3
Jefferson Garcia <jemefox007@gmail.com>
2020-11-24 20:47:40 -0300

    Exercicios03

---
+++ Curso Python/Aula07/Exercicios/Exercicios03.py
@@ -0,0 +1,8 @@
+#Exercicios em Python
+#Faça um programa que calcule a soma entre todos os números que são múltiplos de três e que se encontram no intervalo de 1 até 500.
+
+#Primeiro Exemplo
+
+for cont in range(0, 501):
+    print(cont, end=' ')
+print('Acabou!')
