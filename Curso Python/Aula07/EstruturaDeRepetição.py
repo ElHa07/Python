b7063f048638195595bdd82c4cd333fa3608392d
Jefferson Garcia <jemefox007@gmail.com>
2020-11-24 19:34:40 -0300

    EstruturaDeRepetição
    FOR

--- Curso Python/Aula07/EstruturaDeRepetição.py
+++ Curso Python/Aula07/EstruturaDeRepetição.py
@@ -1,5 +1,12 @@
 # Curso Python #07 - Estrutura de repetição for
 
-for c in range(0,4):
-    print(c)
-print('Acabou!')
+#Primero Exemplo
+#for c in range(0,100,2):
+#    print(c)
+#print('Acabou!')
+
+#Segundo Exemplo
+#for c in range(0, 4):
+#    n = int(input('Digite um Número: '))
+#    print(c)
+#print('FIM!')
