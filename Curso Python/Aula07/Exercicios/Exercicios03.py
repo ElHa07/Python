52020944ef86ed95101c69cf1b0289547e455055
Jefferson Garcia <jemefox007@gmail.com>
2020-11-26 18:55:40 -0300

    Exercicios03

--- Curso Python/Aula07/Exercicios/Exercicios03.py
+++ Curso Python/Aula07/Exercicios/Exercicios03.py
@@ -3,6 +3,10 @@
 
 #Primeiro Exemplo
 
-for cont in range(0, 501):
-    print(cont, end=' ')
-print('Acabou!')
+soma = 0
+c = 0
+for cont in range(0, 500, 2):
+    if cont % 3 == 0:
+        soma += cont
+        c = c + 1
+print('A somos de todos os {} valores solicitados é {}!'.format(c,soma))
