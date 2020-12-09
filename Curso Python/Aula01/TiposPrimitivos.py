c429a1a0623b059cb72a7ad023c885043f8877be
Jefferson Garcia <jemefox007@gmail.com>
2020-12-09 19:47:21 -0300

    Update TiposPrimitivos.py

--- Curso Python/Aula01/TiposPrimitivos.py
+++ Curso Python/Aula01/TiposPrimitivos.py
@@ -2,9 +2,22 @@
 
 #Nessa aula, vamos aprender como funcionam os tipos primitivos no Python e as peculiaridades do int() float() bool() e str(). Além disso, veremos como fazer as primeiras operações com a função print() do Python.
 
-n1 = int(input('Digite um número: '))
-n2 = int(input('Digite mais um número: '))
+#n1 = int(input('Digite um número: '))
+#n2 = int(input('Digite mais um número: '))
 
-s = n1 + n2
+#s = n1 + n2
 
-print('A Soma entre {} e {} vale {} ' .format(n1, n2, s))
+#print('A Soma entre {} e {} vale {} ' .format(n1, n2, s))
+
+#a = 0
+#resultado = 'neutro'
+#if a > 0:
+#    resultado = 'positivo'
+#elif a < 0:
+#    resultado = 'negativo'
+#a = resultado
+
+if not 5==5:
+    print('afirmação verdadeira')
+else:
+    print('afirmação falsa')
