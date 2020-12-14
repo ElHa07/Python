fd025c1f6982d370d50b0a778011b4513b37cf80
Jefferson Garcia <jemefox007@gmail.com>
2020-12-14 20:04:42 -0300

    Metodo

--- Curso Python/Aula08/Exercicios/Exercicios01.py
+++ Curso Python/Aula08/Exercicios/Exercicios01.py
@@ -2,5 +2,4 @@
 
 #Exercício: Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores 'M' ou 'F'. Caso esteja errado, peça a digitação novamente até ter um valor correto.
 
-for x in range(5):
-    resultado = x
+#Primeiro Exemplo
---
+++ Metodos.py
@@ -0,0 +1,24 @@
+#Classic
+#Definição def()
+
+class Calculadora:
+    def __init__(self, num1, num2):
+        self.valor_a = num1
+        self.valor_b = num2
+        
+    def soma(a, b):
+        return a + b
+
+    def subtracao(a, b):
+        return a - b
+
+    def multiplacao(a, b):
+        return a * b
+    def divisao(a, b):
+        return a / b
+
+print(soma(1, 2))
+print(soma(5, 7))
+print(subtracao(5, 7))
+print(multiplacao(5, 6))
+print(divisao(5, 12))
